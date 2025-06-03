c2ad420412e0dd8e6f2a41a25d54710164b482f8
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-03 15:44:01 -0300

    :bulb: docs: adiciona questão 7

---
+++ Algoritmos/Listas-Fabio/Lista-P-FOR-Fabio03/7-soma_numeros_resultado.py
@@ -0,0 +1,9 @@
+def main():
+
+ n = int(input("Digite um número N maior que 0: "))
+
+ for i in range (0, n):
+  resultado = n + 1
+ print(f"A soma dos números entre 1 e {n} é igual a {resultado}")
+
+main()

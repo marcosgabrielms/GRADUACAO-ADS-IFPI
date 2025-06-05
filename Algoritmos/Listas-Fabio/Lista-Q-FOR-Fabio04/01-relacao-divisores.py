b0cd44bbf9c535fb95dc3395e0c5f3c1074e5cc2
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 11:55:28 -0300

    :recycle: refactor: adiciona questão 01
    -> Modifico a lógica de programação python de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/01-relacao-divisores.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/01-relacao-divisores.py
@@ -1,18 +1,18 @@
 def main():
+    while True:
+        numero = int(input("\nDigite um número ou pressione 0 para sair: "))
 
- while True:
-  numero = int(input("\nDigite um número ou pressione 0 para sair: "))
+        if numero == 0:
+            break  # Flag para encerrar o programa
 
-  if numero == 0:
-    break #Flag
-  
-  print(f"\nNúmero: {numero}")
-  print("Divisores:", end=" ") # O end pega o print para terminar com um espaço vazio ao invés de pular a linha
+        print(f"\nNúmero: {numero}")
+        print("Divisores:", end=" ")
 
-  for i in range(1, numero + 1):
-    if numero % i == 0:
-     print(i, end=" ")
+        # Usando os 3 parâmetros do range: início=1, fim=numero+1, passo=1
+        for i in range(1, numero + 1, 1):
+            if numero % i == 0:
+                print(i, end=" ")
 
- print("\n")
+        print("\n")  # quebra de linha após os divisores
 
 main()

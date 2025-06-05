412ceb77d9b1b12b2b28208b5f461dcad27d8fea
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 13:09:28 -0300

    :recycle: refactor: adiciona questão 07
    -> Modifico a lógica de programação em python, de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/07-leitura_numero.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/07-leitura_numero.py
@@ -1,10 +1,10 @@
 def main():
+    n = int(input("Digite o número alvo: "))
 
- n = int(input("Digite o número alvo: "))
+    for _ in range(1000): 
+        num = int(input("Digite um número da lista: "))
+        if num == n:
+            print(f"Número igual ao alvo {n} encontrado.")
+            break
 
- while True:
-  num = int(input("Digite um número da lista "))
-  if num == n:
-   print(f"Número igual ao alvo {n} encontrado. Fim da leitura.")
-   break
 main()

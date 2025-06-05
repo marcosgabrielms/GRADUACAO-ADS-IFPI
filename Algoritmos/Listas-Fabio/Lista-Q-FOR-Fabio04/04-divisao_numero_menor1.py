bd4edc01af18e47562fff6d2bf626e9a5bc5cbe9
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 13:01:55 -0300

    :recycle: refactor: adicona questão 04
    -> Modifico a lógica de programação em python, de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/04-divisao_numero_menor1.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/04-divisao_numero_menor1.py
@@ -1,23 +1,20 @@
 def main():
+    numero = int(input("Digite um número: "))
+    resultados = []
 
- numero = int(input("Digite um número: "))
- resultados = []
+    for n in range(numero, 1, -1):  # começa do número, vai até >1, descendo
+        if n % 2 == 0:  # só divide se for número par (senão não segue a lógica da divisão exata por 2)
+            resultados.append(n)
+            numero = n // 2
+        else:
+            break
 
- while numero > 1:
-    resultados.append(numero)
-    numero = numero // 2
+    for i in range(len(resultados)):
+        if i == len(resultados) - 1:
+            print(f"{resultados[i]}")
+        else:
+            print(f"{resultados[i]} ÷ 2 -> ", end="")
 
- for i in range(len(resultados)):
-   if i == len(resultados) - 1:
-     print(f"{resultados[i]}")
-   else:
-     print(f"{resultados[i]} ÷ 2 -> ", end="")
-
-
-
-
- print(f"O último resultado maior ou igual a 1 foi: {resultados[-1]}")
-
- 
+    print(f"O último resultado maior ou igual a 1 foi: {resultados[-1]}")
 
 main()

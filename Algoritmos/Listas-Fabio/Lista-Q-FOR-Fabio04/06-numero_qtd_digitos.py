9ae87895cf63374987d4c8348b1ca3634ff41b8d
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 13:06:49 -0300

    :recycle: refactor: adiciona questão 06
    -> Modifico a lógica de programação em python, de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/06-numero_qtd_digitos.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/06-numero_qtd_digitos.py
@@ -1,17 +1,15 @@
 def main():
+    numero = int(input("Digite um número: "))
+    numero_inicial = abs(numero)  # usa valor absoluto para evitar número negativo
+    qtd_digitos = 0
 
- numero = int(input("Digite um número: "))
- numero_inicial = abs(numero) # Salva o valor absoluto do número e usa no cálculo, mantendo o número original
+    for _ in range(len(str(numero_inicial))):  # usando for com a quantidade de dígitos da string
+        qtd_digitos += 1
 
- qtd_digitos = 0
+    # Garante que zero tem 1 dígito
+    if numero_inicial == 0:
+        qtd_digitos = 1
 
- while numero_inicial > 0:
-  numero_inicial = numero_inicial // 10
-  qtd_digitos += 1 # Incrementa o contador de dígitos
-  qtd_digitos = qtd_digitos or 1
- 
- 
- print(f"O número {abs(numero)} tem: {qtd_digitos} dígito(s)")
-
+    print(f"O número {abs(numero)} tem: {qtd_digitos} dígito(s)")
 
 main()

ce3b988514b501dd214cee54c05af1563a0d831c
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 12:02:44 -0300

    :reclycle: refactor: adiciona questão 02
    -> Modifico a lógica de programação em python de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/02-mmc-numeros.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/02-mmc-numeros.py
@@ -2,11 +2,12 @@ def main():
     n1 = int(input("Digite o primeiro número: "))
     n2 = int(input("Digite o segundo número: "))
 
-    candidato =  max(n1, n2) 
+    maior = max(n1, n2)
+    limite = n1 * n2  # Valor máximo possível do MMC
 
-    while True:
-        if candidato %  n1 == 0 and candidato % n2 == 0:
-            print(f"O MMC de {n1} e {n2} é: {candidato}") 
+    for candidato in range(maior, limite + 1, 1):  # Começa no maior, vai até n1*n2
+        if candidato % n1 == 0 and candidato % n2 == 0:
+            print(f"O MMC de {n1} e {n2} é: {candidato}")
             break
-        candidato +=1
+
 main()

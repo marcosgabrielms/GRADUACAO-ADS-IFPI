ba65780c8e62e11ab56766f69029f1db08a85607
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 13:03:41 -0300

    :recycle: refactor: adiciona questão 05
    -> Modifico a lógica de programação em python, de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/05-divisao_numero_anterior.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/05-divisao_numero_anterior.py
@@ -1,12 +1,10 @@
 def main():
-
     x = int(input("Digite o valor de X: "))
-    n = int(input("Digite o valor de N sendo >=2: "))
+    n = int(input("Digite o valor de N sendo >= 2: "))
 
-    while n >= 2:
-        resultado = x /n
-        print(f"{x} dividido por {n} = {resultado}")
-        x = resultado
-        n -= 1
+    for i in range(n, 1, -1):  # começa em n, vai até 2 (exclui o 1), de -1 em -1
+        resultado = x / i
+        print(f"{x} dividido por {i} = {resultado}")
+        x = resultado  # atualiza o valor de x para a próxima divisão
 
 main()

6b26d40605c8c0af6e60b85e84290e411f3b7e47
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-05 13:12:22 -0300

    :recycle: refactor: adiciona questão 08
    -> Modifico a lógica de programação em python, de WHILE para FOR

--- Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/08-lista_soma_consecutiva.py
+++ Algoritmos/Listas-Fabio/Lista-Q-FOR-Fabio04/08-lista_soma_consecutiva.py
@@ -5,12 +5,11 @@ def main():
     num = int(input("Digite um número da lista: "))
     lista.append(num)
 
-    while True:
+    for i in range(1, 5, 1):  # start=1, stop=1000, step=1
         proximo = int(input("Digite o próximo número da lista: "))
         lista.append(proximo)
 
-        
-        if lista[-1] + lista[-2] == X: # Verifica se soma dos dois últimos é igual a X
+        if lista[-1] + lista[-2] == X:
             break
 
     print("Números lidos:")

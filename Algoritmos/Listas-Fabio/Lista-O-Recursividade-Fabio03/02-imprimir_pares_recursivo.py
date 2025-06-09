97d43acd9764e24cff3aecda3a64ac201ffa0113
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-09 10:14:23 -0300

    :sparkles: feat: adiciona questão 02

---
+++ Algoritmos/Listas-Fabio/Lista-O-Recursividade-Fabio03/02-imprimir_pares_recursivo.py
@@ -0,0 +1,12 @@
+def imprimir_pares(n):
+    if n == 0:
+        return
+    imprimir_pares(n - 1)
+    if n % 2 == 0:
+        print(n)
+
+print("Digite um número inteiro positivo maior que 1: ")
+n = int(input())
+
+print(f"Números pares de 1 até {n}")
+imprimir_pares(n)

f1d29fbfdb6ec637554874710d4af6b638d34d86
Marcos <marcosgabriel166@gmail.com>
2025-06-13 10:44:27 -0300

    :sparkles: feat: adiciona questão 03

--- Algoritmos/Listas-Fabio/Lista-O-Recursividade-Fabio03/03-progressao_aritmetica.py
+++ Algoritmos/Listas-Fabio/Lista-O-Recursividade-Fabio03/03-progressao_aritmetica.py
@@ -1,4 +1,27 @@
-def progressao_aritimetica(n):
-    if n == 0:
-        return
+def gerar_pa_recursiva(termo_atual, limite, r):
     
+    #Caso Base(condição de parada)
+
+    if termo_atual >= limite:
+        return   # -> Se o termo atual atingiu ou ultrapassou o limite, a função para
+    print(termo_atual)   # Ação
+    gerar_pa_recursiva(termo_atual + r,  limite, r)   # A função chama a si mesma, passando o próximo termo da progressão
+
+
+def main():
+    
+    try:
+        a0 = int(input("Digite o valor inicial de (A0): "))
+        limite =  int(input("Digite o valor do limite: "))
+        r = int(input("Digite o valor da razão (R): "))
+
+        if r <= 0:
+            print("\nErro, a razão (R) deve ser um númeor positivo maior que zero")
+        else:
+            print(f"\n-----Valores da PA menores que {limite}-----")
+            gerar_pa_recursiva(a0,limite,r)
+            print("-------------------------------------------")
+    except ValueError:
+        print("\nErro, por favor, digite apenas números inteiros válidos")
+    
+main()

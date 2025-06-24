0fcbc7be95ff4e87fe67fd6de4bd4540acfd750d
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-24 14:02:26 -0300

    :sparkles: feat: adiciona questão 1050 BeeCrowd

---
+++ Algoritmos/Beecrowd/Bee #01 - Condicionais/bee1050.py
@@ -0,0 +1,21 @@
+def main():
+    ddd = int(input())
+
+    lista_ddd = {
+        61: "Brasilia",
+        71: "Salvador",
+        11: "Sao Paulo",
+        21: "Rio de Janeiro",
+        32: "Juiz de Fora",
+        19: "Campinas",
+        27: "Vitoria",
+        31: "Belo Horizonte"
+    }
+
+    if ddd in lista_ddd:
+        print(lista_ddd[ddd])
+    else:
+        print("DDD nao cadastrado ")
+
+if __name__ == '__main__':
+    main()

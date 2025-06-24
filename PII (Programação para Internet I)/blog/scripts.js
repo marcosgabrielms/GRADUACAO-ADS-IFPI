9fa32a556594a6f5e4ed272c6fccafad43dc74d8
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-24 16:16:10 -0300

    :truck: chore: adiciona atualização ao arquivo JS

--- PII (Programação para Internet I)/blog/scripts.js
+++ PII (Programação para Internet I)/blog/scripts.js
@@ -25,3 +25,32 @@ async function solicitiarConselho() {
     }
     
 }
+
+let botaoPesquisarPais = getById('botaoPesquisarPais');
+
+botaoPesquisarPais.addEventListener("click", PesquisarPais);
+
+async function PesquisarPais() {
+    let url = "https://restcountries.com/v3.1/name/";
+
+    try{
+        let nomePais = getById("nomePais").value;
+        url += nomePais;
+
+        let response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error ('Response status: ${response.status}');
+        }
+        let json = await response.json();
+
+        getById("bandeira").src = json[0].flags.png;
+        getById("bandeira").alt = json[0].flags.alt;
+        getById("nomeOficial").innerText = json[0].name.official;
+       
+
+    } catch(e) {
+        getById("erroPais").innerText = "Erro ao pesquisar país";
+        console.log(e.message);
+    }
+}

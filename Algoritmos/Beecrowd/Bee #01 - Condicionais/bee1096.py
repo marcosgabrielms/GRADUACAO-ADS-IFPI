77d61b72daafdb9481fee9a2c27bcc00e6f2d424
marcosgabrielms <marcosgabriel166@gmail.com>
2025-06-27 13:49:38 -0300

    :sparkles: feat: adiciona questão 1096 BeeCrowd

---
+++ Algoritmos/Beecrowd/Bee #01 - Condicionais/bee1096.py
@@ -0,0 +1,15 @@
+def main():
+    
+    i = 1
+
+    while i <= 9:
+        j = 7
+    
+        while j >= 5:
+
+            print(f"I={i} J={j}")
+            j -= 1
+        i += 2
+
+if __name__ == '__main__':
+   main()

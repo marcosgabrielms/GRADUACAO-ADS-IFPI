936d66937f3a16e4d5db47a8e9c56d3a2d6be55b
Marcos <marcosgabriel166@gmail.com>
2025-07-01 18:07:25 -0300

    :truck: chore: adiciona alterações a lógica de programação
    -> aplicação de filter e lambda

--- Algoritmos/Listas-Fabio/Lista-S-WordPlay/wordplay.py
+++ Algoritmos/Listas-Fabio/Lista-S-WordPlay/wordplay.py
@@ -30,15 +30,15 @@ def main():
             show_words_without_forbidden_char(palavras)
         elif option == 3:
             forbidden_letters = input('Letras Proibidas: ')
-            show_words_without_forbidden_letters(forbidden_letters)
+            show_words_without_forbidden_letters(palavras, forbidden_letters)
         elif option == 4:
             allowed_letter = input('Letras Permitidas: ')
-            show_words_without_allowed_letter(allowed_letter)
+            show_words_without_allowed_letter(palavras, allowed_letter)
         elif option == 5:
             mandatory_letters = input('Letras Obrigatórios: ')
-            show_words_with_mandatory_letters(mandatory_letters)
+            show_words_with_mandatory_letters(palavras, mandatory_letters)
         elif option == 6:
-            show_abecedarian_words()
+            show_abecedarian_words(palavras)
 
         input('>>> continue...')
         clear_screen()
@@ -56,79 +56,50 @@ def show_words_by_size(palavras):
 
 def show_words_without_forbidden_char(palavras):
     forbidden = input('Qual é a letra proibida? ')
-    palavras_filtradas = filtrar(palavras, lambda word: has_no_char_x(word, forbidden))
+    palavras_filtradas = filtrar(palavras, lambda word: forbidden not in word)
     counter = len(palavras)
     filter_counter = len(palavras_filtradas)
     footer(counter, filter_counter)
 
-def has_no_char_x(word, forbidden_char):
-    for char in word:
-        if char == forbidden_char:
-            return False
-    return True
 
-def show_words_without_forbidden_letters(forbidden_letters):
-    with open(FULL_FILE_PATH) as file:
-        counter = 0
-        filter_counter = 0
-        for line in file:
-            word = line.strip()
-            counter += 1
-            if avoids(word, forbidden_letters):
-                print(word)
-                filter_counter += 1
-    footer(counter, filter_counter)
+def show_words_without_forbidden_letters(palavras, forbidden_letters):
+   palavras_filtradas = filtrar(palavras, lambda word: avoids(word, forbidden_letters))
+   show_words(palavras_filtradas)
+   counter = len(palavras)
+   filter_counter = len(palavras_filtradas)
+   footer(counter, filter_counter)
 
-def show_words_with_allowed_letters():
-    allowed_letters = input('Letras Permitidas (usar todas): ')
-    with open(FULL_FILE_PATH) as file:
-        counter = 0
-        filter_counter = 0
-        for line in file:
-            word = line.strip()
-            counter += 1
-            if uses_all(word, allowed_letters):
-                print(word)
-                filter_counter += 1
+def show_words_with_allowed_letters(palavras, allowed_letters):
+    palavras_filtradas = filtrar(palavras, lambda word: uses_all(word, allowed_letters))
+    show_words(palavras_filtradas)
+    counter = len(palavras)
+    filter_counter = len(palavras_filtradas)
     footer(counter, filter_counter)
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def show_words_without_allowed_letter(allowed_letters):
-    with open(FULL_FILE_PATH) as file:
-        counter = 0
-        filter_counter = 0
-        for line in file:
-            word = line.strip()
-            counter += 1
-            if uses_only(word, allowed_letters):
-                print(word)
-                filter_counter += 1
+def show_words_without_allowed_letter(palavras, allowed_letters):
+    palavras_filtradas = filtrar(palavras, lambda word: uses_only(word, allowed_letters))
+    show_words(palavras_filtradas)
+    counter = len(palavras)
+    filter_counter = len(palavras_filtradas)
+    footer(counter, filter_counter)
+    
+
+def show_words_with_mandatory_letters(palavras, mandatory_letters):
+    palavras_filtradas = filtrar(palavras, lambda word: uses_all(word, mandatory_letters))
+    show_words(palavras_filtradas)
+    counter = len(palavras)
+    filter_counter = len(palavras_filtradas)
     footer(counter, filter_counter)
 
-def show_words_with_mandatory_letters(mandatory_letters):
-    with open(FULL_FILE_PATH) as file:
-        counter = 0
-        filter_counter = 0
-        for line in file:
-            word = line.strip()
-            counter += 1
-            if uses_all(word, mandatory_letters):
-                print(word)
-                filter_counter += 1
-    footer(counter, filter_counter)
-
-def show_abecedarian_words():
-    with open(FULL_FILE_PATH) as file:
-        counter = 0
-        filter_counter = 0
-        for line in file:
-            word = to_lower(line.strip())
-            counter += 1
-            if is_abecedarian(word):
-                filter_counter += 1
-                print(word)
+def show_abecedarian_words(palavras):
+    palavras_minusculas =  mapear(palavras, to_lower)
+    palavras_filtradas = filtrar(palavras_minusculas, lambda word:  is_abecedarian(word))
+    show_words(palavras_filtradas)
+    counter = len(palavras)
+    filter_counter = len(palavras_filtradas)
     footer(counter, filter_counter)
 
 def footer(counter, filter_counter):

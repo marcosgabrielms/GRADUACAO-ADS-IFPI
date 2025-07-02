9e82056a4eb63f04acd194af2b4533ea45b1d5f5
marcosgabrielms <marcosgabriel166@gmail.com>
2025-07-02 13:09:23 -0300

    :sparkles: feat: adiciona questão 1144 BeeCrowd

---
+++ Algoritmos/Beecrowd/Bee #02 - Iterações WHILE/FOR/bee1144.py
@@ -0,0 +1,12 @@
+def main():
+    
+    n = int(input())
+
+    for i in range(1, n + 1):
+
+        print(f"{i} {i**2} {i**3}")
+
+        print(f"{i} {i**2 + 1} {i**3 + 1}")
+    
+if __name__ == '__main__':
+    main()
